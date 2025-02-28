af40e9e0f401c8849a0f6e9bfb5b90036d23e840
Huynh Gia Phong Tat <112732639+Jayden-gif-dev@users.noreply.github.com>
2025-02-27 16:41:10 -0800

    data_classes.py missing fake data variables

--- Homework/Homework_03/data_classes.py
+++ Homework/Homework_03/data_classes.py
@@ -47,4 +47,4 @@ if __name__ == "__main__":
         Student(fake.name(), fake.random_int(min=18, max=30), fake.email(), fake.random_int(min=1000, max=9999)).toJson()
         for _ in range(5)
     ]
-    
+#fake data does not work in this case missing variables fake....    
