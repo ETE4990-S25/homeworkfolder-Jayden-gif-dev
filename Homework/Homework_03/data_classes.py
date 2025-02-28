f4a1578d21bbdab6748e58d5c08e8caa43279924
Huynh Gia Phong Tat <112732639+Jayden-gif-dev@users.noreply.github.com>
2025-02-27 16:35:41 -0800

    Start on homework 3

---
+++ Homework/Homework_03/data_classes.py
@@ -0,0 +1,30 @@
+#Imports
+import json
+
+class Person:
+    def __init__(self, Name, Age, Email):
+        self.Name = Name
+        self.Age = Age
+        self.Email = Email
+    
+    def toJson(self):
+        return {
+            "Name": self.Name,
+            "Age": self.Age,
+            "Email": self.Email
+        }
+
+class Student(Person):
+    def __init__(self, Name, Age, Email, StudentId):
+        self.Name = Name
+        self.Age = Age
+        self.Email = Email
+        self.StudentId = StudentId
+    
+    def toJson(self):
+        return {
+            "Name": self.Name,
+            "Age": self.Age,
+            "Email": self.Email,
+            "StudentId": self.StudentId
+        }
